f0b4c7d305d67f0306d984e22123ae27630f0990
Kristján Jökull <kristjanjokull@gmail.com>
2021-04-19 21:40:45 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import commonjs from 'rollup-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
       reactRefresh(),
-      commonjs({
-        ignoreGlobal: true,
-        include: /\/node_modules\//,
-        namedExports: {
-          'react-is': Object.keys(require('react-is')),
-        },
-      }),
     ]
 })
